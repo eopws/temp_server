3012617cf2ae9b7f6ab931eea376e1a182b026f0
xareyli <xareyli@gmail.com>
2023-03-12 19:03:31 +0300

    add try catch

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const accounts = [];
-
-app.get('/get', (req, res) => {
-  res.send(JSON.stringify(accounts))
-})
-
-app.post('/set', jsonParser, (req, res) => {
-    console.log(req.body)
-    accounts.push(req.body);
-
-    res.send('success')
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+try {
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const app = express()
+  const port = 3000
+  
+  const jsonParser = bodyParser.json()
+  const urlencodedParser = bodyParser.urlencoded({ extended: false })
+  
+  const accounts = [];
+  
+  app.get('/get', (req, res) => {
+    res.send(JSON.stringify(accounts))
+  })
+  
+  app.post('/set', jsonParser, (req, res) => {
+      console.log(req.body)
+      accounts.push(req.body);
+  
+      res.send('success')
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+} catch (e) {
+  console.log(e)
+}
